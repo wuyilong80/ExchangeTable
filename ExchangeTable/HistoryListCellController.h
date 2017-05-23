81d646837bbd1c403e0a00dc609c67597af38681
user44 <user44@admin44.local>
2017-05-23 21:16:57 +0800

    20170523 add historylistview

---
+++ ExchangeTable/HistoryListCellController.h
@@ -0,0 +1,13 @@
+//
+//  HistoryListCellController.h
+//  ExchangeTable
+//
+//  Created by user44 on 2017/5/23.
+//  Copyright © 2017年 wuyilong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HistoryListCellController : UITableViewCell
+
+@end
---
+++ ExchangeTable/HistoryListCellController.m
@@ -0,0 +1,24 @@
+//
+//  HistoryListCellController.m
+//  ExchangeTable
+//
+//  Created by user44 on 2017/5/23.
+//  Copyright © 2017年 wuyilong. All rights reserved.
+//
+
+#import "HistoryListCellController.h"
+
+@implementation HistoryListCellController
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
